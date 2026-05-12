7cb751dce9a72c542d31eba210ef9f3a9e036b22
Matthias Metzger <noobymatze@proton.me>
2026-05-12 21:47:57 +0200

    Bump version to 0.1.43

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Whistle.MixProject do
   def project do
     [
       app: :whistle,
-      version: "0.1.41",
+      version: "0.1.43",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
